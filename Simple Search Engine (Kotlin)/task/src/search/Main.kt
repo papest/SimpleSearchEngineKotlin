501619126ad11d629e602d86208089dcd321fbe4
Epap <peia@inbox.ru>
2023-05-04 20:08:15 +0300

    Stage 3/6: User menu

--- Simple Search Engine (Kotlin)/task/src/search/Main.kt
+++ Simple Search Engine (Kotlin)/task/src/search/Main.kt
@@ -2,27 +2,49 @@ package search
 
 const val ENTER_NUMBER_OF_LINES = "Enter the number of people:"
 const val ENTER_ALL_LINES = "Enter all people:"
-const val ENTER_NUMBER_OF_QUERIES = "Enter the number of search queries:"
-const val ENTER_SEARCH_DATA = "Enter data to search people:"
+const val ENTER_SEARCH_DATA = "Enter a name or email to search all suitable people."
 const val RESULT = "People found:"
 const val NOT_FOUND = "No matching people found."
+const val MENU_TEXT = """=== Menu ===
+1. Find a person
+2. Print all people
+0. Exit"""
+const val TITLE_OF_LIST = "=== List of people ==="
+const val INCORRECT_OPTION = "Incorrect option! Try again."
+
+fun searchDialog(lines: List<String>) {
+    val searchData = ENTER_SEARCH_DATA.answer()
+    val result = lines.filter { it.replace("\\s".toRegex(), "").contains(searchData, true) }
+    if (result.isEmpty()) {
+        println(NOT_FOUND)
+    } else {
+        println(RESULT)
+        result.forEach { println(it) }
+    }
+}
+
+fun menu(lines: List<String>) {
+    while (true) {
+        println(MENU_TEXT)
+        when (readln()) {
+            "1" -> searchDialog(lines)
+            "2" -> printAll(lines)
+            "0" -> break
+            else -> println(INCORRECT_OPTION)
+        }
+    }
+}
+
+fun printAll(lines: List<String>) {
+    println(TITLE_OF_LIST)
+    println(lines.joinToString("\n"))
+}
 
 fun expandedSearch() {
     val size: Int = ENTER_NUMBER_OF_LINES.answer().toInt()
     val lines = ENTER_ALL_LINES.answer(size)
-    val countOfQuery = ENTER_NUMBER_OF_QUERIES.answer().toInt()
-
-    repeat(countOfQuery) {
-        val searchData = ENTER_SEARCH_DATA.answer()
-        val result = lines.filter { it.replace("\\s".toRegex(), "").contains(searchData, true) }
-        if (result.isEmpty()) {
-            println(NOT_FOUND)
-        } else {
-            println(RESULT)
-            result.forEach { println(it) }
-        }
-    }
-
+    menu(lines)
+    println("Bye!")
 }
 
 fun String.answer() = also { println(it) }.run { readln() }
