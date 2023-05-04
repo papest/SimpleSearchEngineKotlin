de82350a291bf8b4dfeb08f47b02f2c399a9fea4
Epap <peia@inbox.ru>
2023-05-04 15:52:42 +0300

    Enum

---
+++ Topics/Enum/Days of the week/src/Main.kt
@@ -0,0 +1,13 @@
+enum class DaysOfTheWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
+
+fun main() {
+    for (day in DaysOfTheWeek.values()) println(day)
+}
