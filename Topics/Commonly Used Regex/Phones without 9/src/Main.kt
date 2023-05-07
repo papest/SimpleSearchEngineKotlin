6dcf7b5fc4fc752e20791c3eee4ffd037c7a169b
Epap <peia@inbox.ru>
2023-05-07 11:06:03 +0300

    Phones without 9 only on one line

--- Topics/Commonly Used Regex/Phones without 9/src/Main.kt
+++ Topics/Commonly Used Regex/Phones without 9/src/Main.kt
@@ -1,12 +1,4 @@
-import java.util.*
-
 fun main() {
     val telephoneRegex = "((\\(([0-8]{3})\\))|([0-8]{3}))(-?)[0-8]{3}\\5[0-8]{4}".toRegex()
-    val scanner = Scanner(System.`in`)
-    scanner.useDelimiter("[\\s\n.]")
-
-    while (scanner.hasNext()) {
-        val str = scanner.next()
-        if (str.matches(telephoneRegex)) println(str)
-    }
+    telephoneRegex.findAll(readln()).forEach { println(it.value) }
 }
